03d21b63e16deddb06d97f3a6a10d866100b9ef0
Tali98 <tali_lagos@unitec.edu>
2018-05-04 16:17:14 -0600

    Cree clase Alumnos

--- Lab##2_JoseLagos/src/lab/pkg2_joselagos/Alumnos.java
+++ Lab##2_JoseLagos/src/lab/pkg2_joselagos/Alumnos.java
@@ -12,5 +12,14 @@ import java.util.ArrayList;
  * @author tali_
  */
 public class Alumnos {
-
+     public ArrayList<String> nombre = new ArrayList<>();
+     public ArrayList<String> numero = new ArrayList<>();
+     public ArrayList<String> fecha = new ArrayList<>();
+     public ArrayList<String> edad = new ArrayList<>();
+     public ArrayList<String> residencia = new ArrayList<>();
+     public ArrayList<String> nacionalidad = new ArrayList<>();
+     public ArrayList<String> id = new ArrayList<>();
+     public ArrayList<String> user  = new ArrayList<>();
+     public ArrayList<String> password = new ArrayList<>();
+     public ArrayList<String> notas = new ArrayList<>();
 }
